1a2bce180da7503e8f5f76a2f7434f44187be946
Nick Berejan <njberejan@gmail.com>
2016-04-20 09:44:39 -0400

    fixed several via class discussion

--- textminer/separator.py
+++ textminer/separator.py
@@ -5,19 +5,19 @@ def words(text):
     """no idea why this doesn't work."""
 
 def phone_numbers(text):
-    return re.findall()
+    return re.compile()
 
 
 
 
-@xfail
-@params("input,expected", [
-    ("919-555-1212", {"area_code": "919", "number": "555-1212"}),
-    ("(919) 555-1212", {"area_code": "919", "number": "555-1212"}),
-    ("9195551212", {"area_code": "919", "number": "555-1212"}),
-    ("919.555.1212", {"area_code": "919", "number": "555-1212"}),
-    ("919 555-1212", {"area_code": "919", "number": "555-1212"}),
-    ("555-121", None)
-])
+# @xfail
+# @params("input,expected", [
+#     ("919-555-1212", {"area_code": "919", "number": "555-1212"}),
+#     ("(919) 555-1212", {"area_code": "919", "number": "555-1212"}),
+#     ("9195551212", {"area_code": "919", "number": "555-1212"}),
+#     ("919.555.1212", {"area_code": "919", "number": "555-1212"}),
+#     ("919 555-1212", {"area_code": "919", "number": "555-1212"}),
+#     ("555-121", None)
+# ])
 def test_phone_numbers(input, expected):
     assert s.phone_number(input) == expected
--- textminer/validator.py
+++ textminer/validator.py
@@ -12,9 +12,12 @@ def hex(text):
 def word(text):
     return re.match(r'[18a-zA-Z-]+$', text)
 
-def words(text, count):
-    matches = re.findall(r'[18a-zA-Z-\s]', text)
-    return count == len(matches)
+def words(text, count=None):
+    matches = re.findall(r'[\w-]+[a-zA-Z]+', text)
+    if count is not None:
+        return len(matches) == count
+    else:
+        return len(matches) > 0
 
 def phone_number(text):
     return re.match(r'[\d\W]{3}?[\d\W]{3}?[\d\W]{4}?', text)
@@ -23,8 +26,7 @@ def money(text):
     return re.match(r'^\$([0-9]{1,3})(,?[0-9]{3})*(\.[0-9]{2})?$', text)
 
 def zipcode(text):
-    return re.match(r'^([0-9]{5})(-*)([0-9]{4})*', text)
-    """not sure why this fails, works fine in rubular"""
+    return re.match(r'^([0-9]{5})(\-[0-9]{4})?$', text)
 
 def date(text):
     return re.match(r'[0-9]{1,4}[\/-][0-9]{1,2}[\/-][0-9]{2,4}', text)
