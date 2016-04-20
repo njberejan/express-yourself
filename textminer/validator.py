e6fe227802295d5dc0df4ca0b7c782eb0a6169c5
Nick Berejan <njberejan@gmail.com>
2016-04-19 22:04:08 -0400

    validator done

--- textminer/validator.py
+++ textminer/validator.py
@@ -1,28 +1,4 @@
 import re
-# @xfail
-# def test_words():
-#     """words can take an optional count argument. In case it exists, the text
-#     must match that number of words."""
-#     assert v.words("hello")
-#     assert v.words("hello world")
-#     assert v.words("raggggg hammer dog")
-#     assert v.words("18-wheeler tarbox")
-#     assert v.words("hello", count=1)
-#     assert v.words("hello world", count=2)
-#     assert v.words("raggggg hammer dog", count=3)
-#     assert v.words("18-wheeler tarbox", count=2)
-#     assert not v.words("")
-#     assert not v.words("12")
-#     assert not v.words("hey !!!", count=2)
-#     assert not v.words("bar*us w!zard", count=2)
-#     assert not v.words("hello", count=2)
-#     assert not v.words("hello world", count=3)
-#     assert not v.words("raggggg hammer dog", count=1)
-#     assert not v.words("18-wheeler tarbox", count=3)
-
-
-
-
 
 def binary(text):
     return re.match(r'[01]', text)
@@ -36,9 +12,21 @@ def hex(text):
 def word(text):
     return re.match(r'[18a-zA-Z-]+$', text)
 
-# def words(text, count = None):
-#     a_list = re.findall(r'[18a-zA-Z-\s]', text)
-#     if count:
-#         return re.search(r'[18a-zA-Z-\s]', text), len(a_list)
-#     else:
-#         return re.search(r'[18a-zA-Z-\s]', text)
+def words(text, count):
+    matches = re.findall(r'[18a-zA-Z-\s]', text)
+    return count == len(matches)
+
+def phone_number(text):
+    return re.match(r'[\d\W]{3}?[\d\W]{3}?[\d\W]{4}?', text)
+
+def money(text):
+    return re.match(r'^\$([0-9]{1,3})(,?[0-9]{3})*(\.[0-9]{2})?$', text)
+
+def zipcode(text):
+    return re.match(r'^([0-9]{5})(-*)([0-9]{4})*', text)
+    """not sure why this fails, works fine in rubular"""
+
+def date(text):
+    return re.match(r'[0-9]{1,4}[\/-][0-9]{1,2}[\/-][0-9]{2,4}', text)
+
+#ADVANCE MODE BEGINS#
